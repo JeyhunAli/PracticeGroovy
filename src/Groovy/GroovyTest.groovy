7cfc2a999bc293a8f7ac87da750f05cd2ac7a4f7
Jeyhun <jeyhun_aliyev@partech.com>
2022-02-13 11:37:36 -0500

    new project

---
+++ src/Groovy/GroovyTest.groovy
@@ -0,0 +1,17 @@
+package Groovy
+
+class GroovyTest {
+
+	static void main(args) {
+		print("hey")
+		print(" groovy started ");
+		
+		def name = "jey"
+		print name
+		
+		println 'name is' + name
+		
+		println "name is ${name}"
+		println "name is " + name
+	}
+}
